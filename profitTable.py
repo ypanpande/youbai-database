30e00c2ee4f6bb2b957e3ec811822e35f5db3d95
ypanpande <ypanbx02@gmail.com>
2021-03-08 17:44:03 +0100

    ProfitTable

---
+++ profitTable.py
@@ -0,0 +1,132 @@
+import sqlite3
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
+import pandas as pd
+from pandastable import Table
+import numpy as np
+from datetime import date
+
+class ProfitTable():
+
+
+    #get data from profit table
+    def get_all_data(self, start = '2021-08-01', end = date.today()):
+        df = self.create_profit_table( start =start, end = end)
+
+        tprice = np.sum([float(s) for s in df['price_average']])
+        tpackage_num = np.sum([float(s) for s in df['package_num_average']])
+        tb_price = np.sum([float(s) for s in df['tb_price']])
+        tc_price = np.sum([float(s) for s in df['tc_price']])
+        profit = np.sum([float(s) for s in df['profit']])-tprice
+
+        return {'df': df, 'tprice': tprice, 'tpackage_num': tpackage_num, 'tb_price':tb_price, 'tc_price':tc_price, 'profit':profit }
+
+    # get profit table
+    def create_profit_table(self, start = '2021-08-01', end = date.today()):
+        price_df = pd.read_sql_query('SELECT * FROM product_price', sqlite3.connect('youbai.db'))
+        sql = f'SELECT * FROM goods WHERE day BETWEEN "{start}" AND "{end}"'
+        goods_df = pd.read_sql_query(sql, sqlite3.connect('youbai.db'))
+
+        tem = []
+        for index1, row in goods_df.iterrows():
+            name = row['name']
+            num = row['num']
+            choose_item = price_df[price_df['name'] == name].to_numpy()
+            if len(choose_item) == 0:
+                messagebox.showerror(message = f'add information of "{name}" to "product_price" database')
+                print(name)
+                continue
+
+
+
+            buy_price = float(choose_item[0][1])
+            sell_price = float(choose_item[0][2])
+
+
+            tem.append({'name': name, 'num':num, 'day': row['day'], 'price_average': row['price_average'], 'package_num_average':row['package_num_average'], 'tb_price': buy_price*float(num), 'tc_price' :sell_price*float(num), 'profit':  sell_price*float(num)-buy_price*float(num)})
+
+        return pd.DataFrame(tem)
+
+
+    # get grouped profit table
+    def create_profit_table_group(self, start = '2021-08-01', end = date.today() ):
+        price_df = pd.read_sql_query('SELECT * FROM product_price', sqlite3.connect('youbai.db'))
+        sql = f'SELECT * FROM goods WHERE day BETWEEN "{start}" AND "{end}"'
+        goods_df = self.create_goods_table_group(start, end)
+
+        tem = []
+        for index1, row in goods_df.iterrows():
+            name = row['name']
+            num = row['num']
+            choose_item = price_df[price_df['name'] == name].to_numpy()
+
+            buy_price = float(choose_item[0][1])
+            sell_price = float(choose_item[0][2])
+
+            tem.append({'name': name, 'num':num, 'price_average': row['price_average'], 'package_num_average':row['package_num_average'], 'tb_price': buy_price*float(num), 'tc_price' :sell_price*float(num), 'profit':  sell_price*float(num)-buy_price*float(num)})
+
+        return pd.DataFrame(tem)
+
+    # get grouped goods table
+    def create_goods_table_group(self, start = '2021-08-01', end = date.today()):
+        sql = f'SELECT * FROM goods WHERE day BETWEEN "{start}" AND "{end}"'
+        goods_df = pd.read_sql_query(sql, sqlite3.connect('youbai.db'))
+
+        tem = []
+        for index1, row in goods_df.iterrows():
+            name = row['name']
+            num = float(row['num'])
+            price_average = float(row['price_average'])
+            package_num_average = float(row['package_num_average'])
+            lname = [v['name'] for v in tem]
+            if name in lname:
+                i = lname.index(name)
+                tem[i]['num'] += num
+                tem[i]['price_average'] += price_average
+                tem[i]['package_num_average'] += package_num_average
+            else:
+                ele = {'name':name,'num':num, 'price_average':price_average, 'package_num_average':package_num_average }
+                tem.append(ele)
+
+        return pd.DataFrame(tem)
+
+        # get first 5 rows from grouped profit table
+    def get_sorted_profit_group(self, start = '2021-08-01', end = date.today()):
+
+        df = self.create_profit_table_group().sort_values('profit', ascending = False)
+
+        df5 = df.iloc[:5]
+        name = 'others'
+        num = np.sum(df.iloc[5:]['num'])
+        price_average = np.sum(df.iloc[5:]['price_average'])
+        package_num_average = np.sum(df.iloc[5:]['package_num_average'])
+        tb_price = np.sum(df.iloc[5:]['tb_price'])
+        tc_price = np.sum(df.iloc[5:]['tc_price'])
+        profit = np.sum(df.iloc[5:]['profit'])
+
+        rest_row = pd.DataFrame(data = [{'name':name, 'num':num, 'price_average':price_average, 'package_num_average':package_num_average, 'tb_price':tb_price, 'tc_price':tc_price, 'profit':profit}])
+
+        df6 = pd.concat([df5, rest_row])
+        return df6
+
+
+
+
+
+
+
+def main():
+    root = Tk()
+    f = Frame(root)
+    f.pack()
+
+    df = ProfitTable().create_profit_table()
+    txt = Table(f, dataframe = df)
+    txt.show()
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
