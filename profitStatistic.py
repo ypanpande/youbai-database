dffa4577fc7f9a359a8ba0b0815d0acd1d932f22
ypanpande <ypanbx02@gmail.com>
2021-09-14 19:25:18 +0100

    profitStatistic

---
+++ profitStatistic.py
@@ -0,0 +1,86 @@
+from  tkinter import *
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.backends.backend_tkagg import (
+                                    FigureCanvasTkAgg, NavigationToolbar2Tk)
+from matplotlib.figure import Figure
+
+from profitTable import ProfitTable
+import pandas as pd
+
+class ProfitStatic(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        #coding:utf-8
+        import matplotlib.pyplot as plt
+        plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
+        plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
+
+        fig = Figure(figsize=(10, 10))
+        fig.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.3)
+        self.canvas = FigureCanvasTkAgg(fig, master)  # A
+        self.ax = fig.subplots(1,5)
+
+        self.canvas.get_tk_widget().pack(expand=1, fill = 'both')
+
+
+
+    def test(self):
+        df6 = ProfitTable().get_sorted_profit_group()
+        legend = False
+        names =  {'num':'item numbers', 'price_average': 'delivery price per package', 'tb_price':'total buy price', 'tc_price': 'total sell price', 'profit':'total profit'}
+        for i, colName in enumerate(names.items()):
+            print(colName)
+
+            if i==0:
+                legend = True
+            else:
+                legend = False
+            self.plot_piechart(self.ax[i], df6,colName, legend)
+            # self.plot_bar(self.ax[i], df6,colName)
+
+
+
+
+    def plot_bar(self, ax, df, colName):
+        x = df['name']
+        y = np.round(df[colName[0]],1)
+
+        ax.bar(x, y)
+        ax.set_title(colName[1])
+        ax.set_xticklabels(labels = x, rotation = 90)
+        for index, v in enumerate(y):
+            ax.text(index, v, str(v))
+        self.canvas.draw()
+
+
+    def plot_piechart(self, ax, df, colName, legend = False):
+        name = df['name']
+        y = np.round(df[colName[0]],1)
+
+        autopct='%1.1f%%'
+        ax.pie(y,autopct=autopct)
+        ax.set_title(colName[1])
+        if legend:
+            ax.legend(name).set_draggable(True)
+
+        self.canvas.draw()
+
+
+
+
+
+
+
+
+def main():
+    root = Tk()
+    app = ProfitStatic(root)
+    app.pack(expand = True, fill = 'both')
+
+    app.test()
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
