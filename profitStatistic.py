146d2c88a3d63a30658af909fe92bc407058d95a
ypanpande <ypanbx02@gmail.com>
2021-09-17 19:25:44 +0100

    ans-serif

--- profitStatistic.py
+++ profitStatistic.py
@@ -14,7 +14,7 @@ class ProfitStatic(Frame):
         #coding:utf-8
         import matplotlib.pyplot as plt
         plt.rcParams['font.sans-serif']=['SimHei'] #用来正常显示中文标签
-        plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
+
 
 
         fig = Figure(figsize=(10, 10))
